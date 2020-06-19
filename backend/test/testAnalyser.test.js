6bfe4ebea525322a2a5ff28b05474515951b1877
Sergii Puliaiev <spuliaiev@salesforce.com>
2020-06-18 18:18:41 -0700

    project analyser draft - skipping folders processed before

--- backend/test/testAnalyser.test.js
+++ backend/test/testAnalyser.test.js
@@ -9,8 +9,8 @@ var
 describe('testAnalyser', function() {
   describe('#verifyFileIsTest()', function() {
     it('not a test file', function () {
-      let rootFolder = "module/resources/file.xml";
-      let relativeFileName = "/";
+      let rootFolder = 'module/resources/file.xml';
+      let relativeFileName = '/';
       assert.deepEqual(testAnalyser.verifyFileIsTest(rootFolder, relativeFileName),  {
         ext: '',
         filename: '',
@@ -25,8 +25,8 @@ describe('testAnalyser', function() {
       } );
     });
     it('regular java file - not a test file', function () {
-      let rootFolder = "file.java";
-      let relativeFileName = "module/java/src/";
+      let rootFolder = 'file.java';
+      let relativeFileName = 'module/java/src/';
       assert.deepEqual(testAnalyser.verifyFileIsTest(rootFolder, relativeFileName), {
         ext: '',
         filename: '',
@@ -40,9 +40,9 @@ describe('testAnalyser', function() {
         testFolder: false
       }  );
     });
-    it('java test file', function () {
-      let rootFolder = "/";
-      let relativeFileName = "module/java/src/fileTest.java";
+    it('java test file in Prod module', function () {
+      let rootFolder = '/';
+      let relativeFileName = 'module/java/src/fileTest.java';
       assert.deepEqual(testAnalyser.verifyFileIsTest(rootFolder, relativeFileName), {
         ext: 'java',
         filename: 'fileTest',
@@ -54,6 +54,57 @@ describe('testAnalyser', function() {
         root: '/',
         testFile: true,
         testFolder: false
+      });
+    });
+    it('java test file in Func Test module', function () {
+      let rootFolder = '/';
+      let relativeFileName = 'module/test/func/java/src/fileTest.java';
+      assert.deepEqual(testAnalyser.verifyFileIsTest(rootFolder, relativeFileName), {
+        ext: 'java',
+        filename: 'fileTest',
+        lang: 'java',
+        module: 'module',
+        modulePath: 'module/test/func/java',
+        ownershipFilePath: 'module/test/func/java/java/resources/ownership.yaml',
+        relative: 'module/test/func/java/src/fileTest.java',
+        root: '/',
+        testFile: true,
+        testFolder: true,
+        testKind: 'func'
+      });
+    });
+    it('java test file in Unit Test module', function () {
+      let rootFolder = '/';
+      let relativeFileName = 'module/test/unit/java/src/unit/fileTest.java';
+      assert.deepEqual(testAnalyser.verifyFileIsTest(rootFolder, relativeFileName), {
+        ext: 'java',
+        filename: 'fileTest',
+        lang: 'java',
+        module: 'module',
+        modulePath: 'module/test/unit/java',
+        ownershipFilePath: 'module/test/unit/java/java/resources/ownership.yaml',
+        relative: 'module/test/unit/java/src/unit/fileTest.java',
+        root: '/',
+        testFile: true,
+        testFolder: true,
+        testKind: 'unit'
+      });
+    });
+    it('java test file in StrictUnit Test module', function () {
+      let rootFolder = '/';
+      let relativeFileName = 'module/test/unit/java/src/strictunit/fileTest.java';
+      assert.deepEqual(testAnalyser.verifyFileIsTest(rootFolder, relativeFileName), {
+        ext: 'java',
+        filename: 'fileTest',
+        lang: 'java',
+        module: 'module',
+        modulePath: 'module/test/unit/java',
+        ownershipFilePath: 'module/test/unit/java/java/resources/ownership.yaml',
+        relative: 'module/test/unit/java/src/strictunit/fileTest.java',
+        root: '/',
+        testFile: true,
+        testFolder: true,
+        testKind: 'strictunit'
       });
     });
   });
